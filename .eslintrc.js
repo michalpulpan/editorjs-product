bd3f4763f9baeda460c3c20ec4aa51c1ea04f557
Michal Pulpan <me@michalpulpan.eu>
2022-07-15 16:18:58 +0200

    builds

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: 'airbnb-base',
+    plugins: ['jest'],
+    rules: {
+      'class-methods-use-this': ['off'],
+      'no-underscore-dangle': ['error', { allowAfterThis: true }],
+      'no-unused-expressions': [2, { allowTernary: true }],
+    },
+    env: {
+      browser: true,
+      'jest/globals': true,
+    },
+  };
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,44 @@
+const path = require('path');
+
 module.exports = {
-  mode: 'production',
+  entry: './src/index.js',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader', options: { attributes: { id: 'editorjs-product' } } },
+          { loader: 'css-loader' },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-inline-loader',
+            options: {
+              removeSVGTagAttrs: false,
+            },
+          },
+        ],
       },
     ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
   output: {
-    filename: 'index.js',
-    library: 'EditorJSStyle',
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+    library: 'Product',
     libraryTarget: 'umd',
+    libraryExport: 'default',
   },
 };
