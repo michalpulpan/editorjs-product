e4a8ca3bb38c186bed47462314c5b83cda8f8dd1
Michal Pulpan <me@michalpulpan.eu>
2022-07-15 17:00:14 +0200

    lol export

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-      // {
-        // test: /\.css$/,
-        // use: [
-          // { loader: 'style-loader', options: { attributes: { id: 'editorjs-tooltip' } } },
-          // { loader: 'css-loader' },
-        // ],
-      // },
-      // {
-        // test: /\.svg$/,
-        // use: [
-        //   {
-        //     loader: 'svg-inline-loader',
-        //     options: {
-        //       removeSVGTagAttrs: false,
-        //     },
-        //   },
-        // ],
-      // },
-    ],
-  },
   output: {
     path: path.join(__dirname, '/dist'),
     publicPath: '/',
-    filename: 'bundle.js',
-    library: 'ProductEditorJs',
-    libraryTarget: 'umd',
+    filename: 'product.js',
+    library: 'Product',
     libraryExport: 'default',
+    libraryTarget: 'umd'
   },
 };
